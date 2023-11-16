fd34befa2c05d98515760c53e4b5824fe878e27e
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2023-11-15 22:41:43 -0300

    s

--- js/script.js
+++ js/script.js
@@ -1,2 +1,46 @@
-// Add some interactivity to the website
-console.log("Hello, world!");
+const cgTheme = document.querySelector('#change-theme');
+
+let lightMode = true;
+const rootElement = document.documentElement;
+
+const lightTheme = {
+    '--background-color': '#F2F9FE',
+    '--text-color': '#000000',
+    '--navigator-item-color': '#667488',
+    '--navigator-hover-color': 'black',
+    '--sun-icon-fill': '#223343',
+    '--sun-icon-stroke':' #223343',
+    '--sun-icon-background-color':'white',
+    '--moon-icon-fill':'white',
+    '--moon-icon-background-color':'#223343',
+    '--alarado-logo-color': '#223343'
+};
+
+const darkTheme = {
+    '--background-color': '#111629',
+    '--text-color': '#FFFFFF',
+    '--navigator-item-color': '#FFFFFF',
+    '--navigator-hover-color': 'white',
+    '--sun-icon-fill': 'white',
+    '--sun-icon-stroke':' white',
+    '--sun-icon-background-color':'#223343',
+    '--moon-icon-fill':'#223343',
+    '--moon-icon-background-color':'white',
+    '--alarado-logo-color': 'white'
+};
+
+cgTheme.addEventListener('click',function(){
+   lightMode ? changeTheme(darkTheme) : changeTheme(lightTheme);
+   lightMode = !lightMode; 
+   
+});
+
+function changeTheme(theme){
+    for (prop in theme){
+        changeProperty(prop, theme[prop])
+    }
+}
+
+function changeProperty(property, value){
+    rootElement.style.setProperty(property,value);
+}
